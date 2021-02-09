e49b164581105aa8e25bd317bf64c52f6f5916f4
Southseast <Southseast@users.noreply.github.com>
2021-02-09 04:34:46 +0000

    deploy: AmemachiF/Killer_Bar@7d7bda469a1340f063ca1c54207f8348846f3327

--- _nuxt/static/1612842275/observation/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002FKiller_Bar.pages\u002F_nuxt\u002Fstatic\u002F1612842275",layout:"default",error:null,serverRendered:a,routePath:"\u002Fobservation",config:{},colorMode:{preference:b,value:b,unknown:a,forced:false}}}(true,"system"));
